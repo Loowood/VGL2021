78e1640e3682fb3b2c01487687f9a2e74701f302
scartoning2020 <scarton.ing2020@esaip.org>
2021-11-06 10:20:37 +0100

    change pusher to work without rigidbody

--- Assets/Scripts/PusherWithRigidBody.cs
+++ Assets/Scripts/PusherWithRigidBody.cs
@@ -2,48 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PusherMoves : MonoBehaviour
+public class PusherWithRigidBody : MonoBehaviour
 {
     [SerializeField]
     bool advance = true;
+    Rigidbody rigidbody;
     public float baseChrono = 20;
     float chrono;
     public float speed = 1.0f;
-    Vector3 movingDirection;
 
     private void Awake()
     {
-        //rigidbody = gameObject.GetComponent<Rigidbody>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
         chrono = baseChrono;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (advance)
-        {
-            movingDirection = Vector3.forward;
-        }
-        else
-        {
-            movingDirection = Vector3.back;
-        }
-        gameObject.transform.Translate(movingDirection * speed * Time.deltaTime);
+        float direction = (advance) ? 1f : -1f;
+        rigidbody.AddForce(new Vector3(0f, 0f, (direction * speed * rigidbody.mass)));
         chrono -= Time.deltaTime;
         if (gameObject.transform.position.z > -8 && advance)
         {
             advance = false;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
             chrono = baseChrono;
         }
         if (gameObject.transform.position.z < -13 && !advance)
         {
             advance = true;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
             chrono = baseChrono;
         }
     }
