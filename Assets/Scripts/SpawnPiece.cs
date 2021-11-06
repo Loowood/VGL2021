55368b092705cc15042726d49b522bde11710930
Carton Samuel <scarton.ing2020@esaip.org>
2021-11-06 18:51:02 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/SpawnPiece.cs
+++ Assets/Scripts/SpawnPiece.cs
@@ -9,6 +9,13 @@ public class SpawnPiece : MonoBehaviour
 
     public float leftestPos = -3f;
     public float rightestPos = 3f;
+    public bool moveRight = true;
+    public float speed = 1f;
+    Vector3 movingDirection;
+    float maxRightPosition;
+    float maxLeftPosition;
+    float maxPos;
+    float minPos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +24,27 @@ public class SpawnPiece : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        if (moveRight)
+        {
+            movingDirection = Vector3.forward;
+        }
+        else
+        {
+            movingDirection = Vector3.back;
+        }
+        gameObject.transform.Translate(movingDirection * speed * Time.deltaTime);
+        if (gameObject.transform.position.z > maxPos && moveRight)
+        {
+            moveRight = false;
+        }
+        if (gameObject.transform.position.z < minPos && !moveRight)
+        {
+            moveRight = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && JetonManager.instance.getNbJetons() > 0)
         {
             if (posAleatoire)
