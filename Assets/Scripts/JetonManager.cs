412b16e076824d44d0e894cd073f8d1bf29ced9e
Loowood <lorisg01@yahoo.fr>
2021-11-06 17:47:18 +0100

    Castle Scene post processing & Lights

--- Assets/Scripts/JetonManager.cs
+++ Assets/Scripts/JetonManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JetonManager : MonoBehaviour
 {
 	public static JetonManager instance;
 	[SerializeField]
 	private int nbJetons;
-
+	public Text jetonsText;
 
 	void Awake()
 	{
@@ -22,22 +23,31 @@ public class JetonManager : MonoBehaviour
 		}
 	}
 
-	public void addJeton(int nb)
+    private void Start()
+    {
+		nbJetons = 20;
+		jetonsText.text = nbJetons + " jetons";
+    }
+
+    public void addJeton(int nb)
     {
 		nbJetons += nb;
-    }
+		jetonsText.text = nbJetons + " jetons";
+	}
 
 	public void removeJeton(int nb)
     {
 		nbJetons -= nb;
 		if (nbJetons < 0)
 			nbJetons = 0;
-    }
+		jetonsText.text = nbJetons + " jetons";
+	}
 
 	public void setNbJetons(int nb)
     {
 		nbJetons = nb;
-    }
+		jetonsText.text = nbJetons + " jetons";
+	}
 
 	public int getNbJetons()
     {
